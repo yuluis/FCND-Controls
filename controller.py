add moment of inertia and fix Euler rotation application.

controller.py
@@ -136,30 +136,34 @@ class NonlinearController(object):
             
         Returns: 2-element numpy array, desired rollrate (p) and pitchrate (q) commands in radians/s
         """
+        k_p_rollpitch = 1
 
         self.rot_mat = euler2RM (acceleration_cmd[0], acceleration_cmd[1],thrust_cmd)
         b_x = self.rot_mat[0, 2]
-        b_x_err = attitude[0] - b_x
-        k_p_rollpitch = 1
-
+        b_x_err = b_x - attitude[0]
         b_x_p_term = k_p_rollpitch * b_x_err
 
         b_y = self.rot_mat[1, 2]
-        b_y_err = attitude[1] - b_y
+        b_y_err = b_y - attitude[1]
         b_y_p_term = k_p_rollpitch * b_y_err
-
 
         b_x_commanded_dot = b_x_p_term
         b_y_commanded_dot = b_y_p_term
 
-
         rot_mat1=np.array([[self.rot_mat[1,0],-self.rot_mat[0,0]],[self.rot_mat[1,1],-self.rot_mat[0,1]]])/self.rot_mat[2,2]
-        rot_rate = np.matmul(rot_mat1,np.array([b_x_commanded_dot,b_y_commanded_dot]).T)
-        tau=2*DRONE_MASS_KG* 1
-        p_c = rot_rate[0]/tau
-        q_c = rot_rate[1]/tau
 
-        #print("time= {0:.4f}, accel cmd, attitude, thrust_cmd, p_c, q_c)".format(timer.time()), acceleration_cmd, attitude, thrust_cmd, np.array([p_c, q_c]) )
+        rot_rate = np.matmul(rot_mat1,np.array([b_x_commanded_dot,b_y_commanded_dot]).T)
+        tau=2*DRONE_MASS_KG* 0.3
+        p_c = rot_rate[0]/tau * MOI[0] # TODO factor in rotational moment? are p and q body frame moments?
+        q_c = rot_rate[1]/tau * MOI[1]
+
+
+        print("roll_pitch_controller:: time= {0:.4f}, accel cmd, attitude, thrust_cmd, p_c, q_c)".format(timer.time()),
+              acceleration_cmd, attitude, thrust_cmd, np.array([p_c, q_c]) )
+        if attitude[0] > 1 :
+            print("attitude", attitude)
+            pass
+
         return np.array([p_c, q_c])
 
     def body_rate_control(self, body_rate_cmd, body_rate):
@@ -169,18 +173,17 @@ class NonlinearController(object):
             body_rate_cmd: 3-element numpy array (p_cmd,q_cmd,r_cmd) in radians/second^2
             attitude: 3-element numpy array (p,q,r) in radians/second^2
         """
-        body_p = 0.3
+        body_p = 1
 
-        u_bar_p = (body_rate_cmd[0] - body_rate[0]) * body_p
+        u_bar_p = (body_rate_cmd[0] - body_rate[0]) * body_p * MOI[0]
 
-        u_bar_q = (body_rate_cmd[1] - body_rate[1]) * body_p
+        u_bar_q = (body_rate_cmd[1] - body_rate[1]) * body_p * MOI[1]
 
-        u_bar_r = (body_rate_cmd[2] - body_rate[2]) * body_p
+        u_bar_r = (body_rate_cmd[2] - body_rate[2]) * body_p * MOI[2]
 
         body_new = np.array([u_bar_p, u_bar_q, u_bar_r])
 
-        print("time= {0:.4f}, body_rate (cmd, curr, new)".format(timer.time()), body_rate_cmd, body_rate, body_new )
-
+        print("body_rate_control:: time= {0:.4f}, body_rate (cmd, curr, new)".format(timer.time()), body_rate_cmd, body_rate, body_new )
 
         #Returns: 3-element numpy array, desired roll moment, pitch moment, and yaw moment commands in Newtons*meters
         return body_new

controls_flyer.py
@@ -69,46 +69,59 @@ class ControlsFlyer(UnityDrone):
                 self.local_velocity_target[0:2],
                 self.local_position[0:2],
                 self.local_velocity[0:2])
-        self.local_acceleration_target = np.array([acceleration_cmd[0],
-                                                   acceleration_cmd[1],
-                                                   0.0])
+#        self.local_acceleration_target = np.array([acceleration_cmd[0],
+#                                                   acceleration_cmd[1],
+#                                                   0.0])
+        # TODO zero out accelleration for development
+        self.local_acceleration_target = np.array([0.0,0.0, 0.0])
 
     def attitude_controller(self):
 
         # do this every 4 message for about 100 ms
         self.delaycounter = self.delaycounter + 1
-        if self.delaycounter % 8 == 0 :
-            self.thrust_cmd = self.controller.altitude_control(
-                    -self.local_position_target[2],
-                    -self.local_velocity_target[2],
-                    -self.local_position[2],
-                    -self.local_velocity[2],
-                    self.attitude,
-                    0) #TODO set acceleration to 0 to debug
+        #if self.delaycounter % 8 == 0 :
+        self.thrust_cmd = self.controller.altitude_control(
+                -self.local_position_target[2],
+                -self.local_velocity_target[2],
+                -self.local_position[2],
+                -self.local_velocity[2],
+                self.attitude,
+                0) #TODO set acceleration to 0 to debug
 
-        if self.delaycounter % 4 == 0 :
-            self.roll_pitch_rate_cmd = self.controller.roll_pitch_controller(
-                    # self.local_acceleration_target[0:2], # TODO for now not implment positiona
-                    self.roll_pitch_rate_cmd,
-                    self.attitude,
-                    self.thrust_cmd)
+        #if self.delaycounter % 4 == 0 :
+        self.roll_pitch_rate_cmd = self.controller.roll_pitch_controller(
+                # self.local_acceleration_target[0:2], # TODO for now not implment positiona
+                self.roll_pitch_rate_cmd,
+                self.attitude,
+                self.thrust_cmd)
 
         yawrate_cmd = self.controller.yaw_control(
-                #self.attitude_target[2], #TODO as zero for now
-                0,
+                self.attitude_target[2],
                 self.attitude[2])
 
         self.body_rate_target = np.array(
                 [self.roll_pitch_rate_cmd[0], self.roll_pitch_rate_cmd[1], yawrate_cmd])
-        
+        #TODO zero out the body_rate_target request
+        print("attitude_controller:: roll, pitch, yaw (world)", self.body_rate_target)
+        #self.body_rate_target = np.array([1,0,0])
+
     def bodyrate_controller(self):        
         moment_cmd = self.controller.body_rate_control(
                 self.body_rate_target,
                 self.gyro_raw)
+#        self.cmd_moment(moment_cmd[0],
+#                        moment_cmd[1],
+#                        moment_cmd[2],
+#                        self.thrust_cmd)
+
+        # add debugging to hover only TODO
+        print("bodyrate_controller: bodyrate moment, thrust", moment_cmd, self.thrust_cmd)
         self.cmd_moment(moment_cmd[0],
                         moment_cmd[1],
                         moment_cmd[2],
-                        self.thrust_cmd)
+                        9.81/2)
+        pass
+
     
     def attitude_callback(self):
         if self.flight_state == States.WAYPOINT: